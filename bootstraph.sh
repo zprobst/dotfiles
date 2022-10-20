53fe95a648005122e079de0b41d15d4d6d9da3c8
Zach Probst <Zach_Probst@intuit.com>
2022-10-20 10:18:25 -0700

    Adding git command to pull in the theme and fix bug

--- bootstraph.sh
+++ bootstraph.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Install Oh-my-ZSH
+# Install Oh-my-ZSH and preferred theme
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Remove template .zshrc file so we can symlink it later
+rm ~/.zshrc
 
 # Symlink to the appropriate places home folder
 ln -s ~/dotfiles/.zshrc ~/.zshrc 
